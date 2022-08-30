fc025fc3692a954109c55a12378532b26c9d8114
AnneKammenhed <anne.juriado@gmail.com>
2022-08-30 12:56:23 +0000

    add initian bookings model

--- food/models.py
+++ food/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# Model for the restaurants 20 tables, max online booking 8 guests
+
+class Bookings(models.Model):
+    booking_guests = models.IntegerField()
+    booking_time = models.DateTimeField()
+    guest_email = models.EmailField()
+    guest_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking")
+    allergies = models.TextField(max_length=200)
+    number_of_tables = models.IntegerField()
+
+    def __str__(self):
+        return f'Booking for {self.booking_guests} guests at {self.booking_time}'
