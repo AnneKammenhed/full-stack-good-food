a9f6f3d50dd73fad9f1b6a3ace90e74e69e74190
AnneKammenhed <anne.juriado@gmail.com>
2022-08-31 07:30:47 +0000

    try to get html pages up and working

--- food/views.py
+++ food/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views import generic
 from .models import Booking
 
 # Create your views here:
 # view to se the Bookings model
-class BookingList(ListView):
+class BookingList(generic.ListView):
     model = Booking
