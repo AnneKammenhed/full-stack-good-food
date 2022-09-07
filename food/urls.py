1e9951b23f14a9c3b5753122abd8c382ea3a4e82
AnneKammenhed <anne.juriado@gmail.com>
2022-09-07 10:42:32 +0000

    add new first urls and views

--- food/urls.py
+++ food/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 
-# urlpatterns = [
-#     path('', views.BookingList.as_view(), name="home"),
-# ]
+urlpatterns = [
+    path('', views.home, name="home"),
+]
--- food/views.py
+++ food/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from django.views import generic
-from .models import Booking
-
-# view to see the form
 
 
-# view to book a table
-
-
-# view to delete a booking (user login required)
+def home(request):
+    return render(request, 'index.html', {})
