8f6f36016b0e8cf269c6b8ee87834d2aa9916664
AnneKammenhed <anne.juriado@gmail.com>
2022-09-07 11:53:09 +0000

    get html urls and pages set up

--- food/urls.py
+++ food/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('menu', views.menu, name="menu"),
+    path('bookings', views.bookings, name="bookings"),
 ]
--- food/views.py
+++ food/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'index.html', {})
+    name = "Anne"
+    return render(
+        request, 
+        'index.html', {
+        "name": name,
+    },)
+
+def menu(request):
+    return render(
+        request, 
+        'menu.html',)
+
+def bookings(request):
+    return render(
+        request,
+        'bookings.html',)
